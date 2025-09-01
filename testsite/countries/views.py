3fe640da9155009116ff664b00248055fcd9232e
varun.dunga <varun.dunga@builder.ai>
2025-09-01 23:01:35 +0100

    exercise 1

--- testsite/countries/views.py
+++ testsite/countries/views.py
@@ -1,8 +1,16 @@
 from django.http import JsonResponse
+from .models import Region
 
 
 def stats(request):
-    # TODO - Provide name, number_countries and total_population for each region
-    response = {"regions": []}
+    regions_data = []
+    for region in Region.objects.all():
+        countries = region.countries.all()
+        regions_data.append({
+            "name": region.name,
+            "number_countries": countries.count(),
+            "total_population": sum(c.population for c in countries)
+        })
+    response = {"regions": regions_data}
 
     return JsonResponse(response)
